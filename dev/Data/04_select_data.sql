7d5b28b7112391e4c8be974b337a31e9ef314a72
bhalla008 <bhalla.jatin008@gmail.com>
2025-12-14 02:44:05 +0530

    Update SQL query to select project and employee IDs
    Replaced previous employee selection with project and employee IDs.

--- dev/Data/04_select_data.sql
+++ dev/Data/04_select_data.sql
@@ -1,7 +1,8 @@
 USE olfm_db;
 
-SELECT * FROM employees;
-
-SELECT e.emp_name, e.department, p.project_name
-FROM employees e
-JOIN projects p ON e.emp_id = p.emp_id;
+SELECT 
+    p.project_id,
+    e.emp_id
+FROM projects p
+JOIN employees e
+  ON p.emp_id = e.emp_id;
